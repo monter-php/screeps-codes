b4052edd0774ff9073536cca5729d5a6251c27da
Grzegorz Trukan <grzegorz.trukan@huuugegames.com>
2018-07-11 10:28:09 +0200

    path test

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ module.exports.loop = function () {
     if(Game.spawns['Spawn1'].spawning) { 
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
-            'ð ï¸' + spawningCreep.memory.role,
+            spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1, 
             Game.spawns['Spawn1'].pos.y, 
             {align: 'left', opacity: 0.8});
--- role.harvester.js
+++ role.harvester.js
@@ -3,9 +3,10 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            const target = creep.pos.findClosestByPath(FIND_SOURCES);
+            //var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {

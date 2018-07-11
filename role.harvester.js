3f2903667bbfd1c74b38ecbb5f85b4b317ab4d43
Grzegorz Trukan <grzegorz.trukan@huuugegames.com>
2018-07-11 11:11:07 +0200

    harvester updates

--- role.harvester.js
+++ role.harvester.js
@@ -14,13 +14,21 @@ var roleHarvester = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy <= structure.energyCapacity;
+                            structure.energy < structure.energyCapacity;
                     }
             });
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            } else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return structure.structureType == STRUCTURE_SPAWN;
+                    }
+                });
+
+                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#0000ff'}});
             }
         }
 	}
